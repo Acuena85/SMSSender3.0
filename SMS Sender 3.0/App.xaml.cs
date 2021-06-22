e8443f44af4e3a2af1929f8e770776ae9853cab7
JohanPA <62965652+JohanPA@users.noreply.github.com>
2021-06-22 08:43:00 +0200

    Initial Commit

---
+++ SMS Sender 3.0/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SMS_Sender_3._0
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ SMS Sender 3.0/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace SMS_Sender_3._0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
